7f5a15933bb0f8a6d7733faba6f3ce0f9f1d374c
Uriel Agüero <uriel.aguero1812@gmail.com>
2023-05-18 17:31:08 -0300

    Preentrega coder

---
+++ app/agregar-registro.js
@@ -0,0 +1,104 @@
+const botonAbrir = document.querySelector('.b-a');
+const botonCerrar = document.querySelector('.cancelar-formulario');
+const botonAgregar = document.querySelector('.completar-formulario');
+const inputNombre = document.querySelector('.input-nombre');
+const inputRol = document.querySelector('.input-rol');
+const overlay = document.querySelector('.overlay');
+
+
+//Abrir formulario
+botonAbrir.addEventListener('click', function(){
+    overlay.style.visibility = 'visible';
+    overlay.style.opacity = '1';
+    inputNombre.value = '';
+    inputNombre.style.border = 'none';
+    inputRol.value = '';
+    inputRol.style.border = 'none';
+});
+
+
+
+//Cerrar formulario
+botonCerrar.addEventListener('click', function(){
+    overlay.style.visibility = 'hidden';
+    overlay.style.opacity = '0';
+    inputNombre.value = '';
+    inputNombre.style.border = 'none';
+    inputRol.value = '';
+    inputRol.style.border = 'none';
+});
+
+let Vnom;
+let Vrol;
+
+//Hacer registro con enter, es re croto hacerlo asi pero no tengo tiempo para buscar la forma :(
+window.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        botonAgregar.click();
+    }  
+});
+    
+botonAgregar.addEventListener('click', (e)=>{
+    //Validación de campo
+    if(!inputNombre.value){
+        inputNombre.style.border = '1px solid red';
+    }else{
+        Vnom = inputNombre.value
+    }
+
+    if(!inputRol.value){
+        inputRol.style.border = '1px solid red';
+    }else{
+        Vrol = inputRol.value
+        if(!JSON.parse(localStorage.getItem('roles')).find((e)=> e.toLowerCase() === Vrol.toLocaleLowerCase())){
+            let roles = JSON.parse(localStorage.getItem('roles'));
+            roles.push(Vrol);
+            localStorage.setItem('roles', JSON.stringify(roles));
+
+            //Actualizar lista visible
+            containerRoles.innerHTML = '';
+            JSON.parse(localStorage.getItem('roles')).forEach(rol =>{
+                let InfoPersona = document.createElement('div');
+                InfoPersona.classList.add('item');
+                InfoPersona.textContent = rol;
+                containerRoles.appendChild(InfoPersona);
+            });
+        }
+    }
+
+    let today = new Date();
+    if(Vnom && Vrol){
+        //Agregar el registro
+        let lista = JSON.parse(localStorage.getItem('lista'));
+        let cantidadIDs = lista.length;
+
+        let day = today.getDate();
+        let month = today.getMonth() + 1;
+        let year = today.getFullYear()
+
+        let registro = {
+            id: cantidadIDs+1,
+            nombre: Vnom,
+            rol: Vrol,
+            fecha_reg: `${day}/${month}/${year}`
+        }
+        lista.push(registro);
+        localStorage.setItem('lista', JSON.stringify(lista));
+
+        overlay.style.visibility = 'hidden';
+        overlay.style.opacity = '0';
+        inputNombre.value = '';
+        inputNombre.style.border = 'none';
+        inputRol.value = '';
+        inputRol.style.border = 'none';
+
+        //Actualizar lista visible
+        container.innerHTML = '';
+        JSON.parse(localStorage.getItem('lista')).forEach(persona =>{
+            let InfoPersona = document.createElement('div');
+            InfoPersona.classList.add('item');
+            InfoPersona.textContent = persona.nombre;
+            container.appendChild(InfoPersona);
+        });
+    }
+});
--- app/index.js
+++ app/index.js
@@ -1,56 +1,74 @@
 //Simulación de base de datos
 const persona1 = {
-    id: 1,
+    id: 2,
     nombre: 'Aitor Tilla',
-    rol: 'Cliente'
+    rol: 'Cliente',
+    fecha_reg: '10/08/2000'
 }
 const persona2 = {
     id: 8,
     nombre: 'Alba Sura',
-    rol: 'Cliente'
+    rol: 'Cliente',
+    fecha_reg: '10/08/2000'
 }
 const persona3 = {
     id: 5,
     nombre: 'Alex Cremento',
-    rol: 'Empleado'
+    rol: 'Empleado',
+    fecha_reg: '02/05/1995'
 }
 const persona4 = {
     id: 4,
     nombre: 'Ana Busado de Hesa',
-    rol: 'Empleado'
+    rol: 'Empleado',
+    fecha_reg: '02/05/1995'
 }
 const persona5 = {
     id: 3,
     nombre: 'Andrés Trozado',
-    rol: 'Cliente'
+    rol: 'Cliente',
+    fecha_reg: '10/08/2000'
 }
 const persona6 = {
     id: 6,
     nombre: 'Armando Bronca Segura',
-    rol: 'Empleado'
+    rol: 'Empleado',
+    fecha_reg: '02/05/1995'
 }
 const persona7 = {
     id: 7,
     nombre: 'Dolores Delano',
-    rol: 'Cliente'
+    rol: 'Cliente',
+    fecha_reg: '10/08/2000'
 }
 const persona8 = {
-    id: 2,
+    id: 1,
     nombre: 'Elba Surero',
-    rol: 'Jefe'
+    rol: 'Jefe',
+    fecha_reg: '02/05/1995'
 }
 const personas = [persona1, persona2, persona3, persona4, persona5, persona6, persona7, persona8];
+const roles = ["Jefe", "Empleado", "Cliente"];
+//La almaceno en el localStorage
+localStorage.setItem('lista', JSON.stringify(personas));
+localStorage.setItem('roles', JSON.stringify(roles));
 
 //Mostrando personas
 const container = document.querySelector('.lista-personas');
-
-personas.forEach(persona =>{
-
+JSON.parse(localStorage.getItem('lista')).forEach(persona =>{
     let InfoPersona = document.createElement('div');
     InfoPersona.classList.add('item');
     InfoPersona.textContent = persona.nombre;
     container.appendChild(InfoPersona);
-    
+});
+
+//Mostrando roles
+const containerRoles = document.querySelector('.lista-roles');
+JSON.parse(localStorage.getItem('roles')).forEach(rol =>{
+    let InfoPersona = document.createElement('div');
+    InfoPersona.classList.add('item');
+    InfoPersona.textContent = rol;
+    containerRoles.appendChild(InfoPersona);
 });
 
 //Accion de botones
@@ -64,11 +82,12 @@ btnNombre.addEventListener('click', (e)=>{
     let EntradaNombre = prompt('Ingresar nombre');
 
     if(EntradaNombre != null || EntradaNombre != undefined){
-        let InfoRequerida = personas.find((e)=> e.nombre.toLowerCase() === EntradaNombre.toLocaleLowerCase());
+        let InfoRequerida = JSON.parse(localStorage.getItem('lista')).find((e)=> e.nombre.toLowerCase() === EntradaNombre.toLocaleLowerCase());
         resultado.innerHTML = `
         Nombre: <b>${InfoRequerida.nombre}</b><br>
         ID: ${InfoRequerida.id}<br>
-        Rol: ${InfoRequerida.rol}
+        Rol: ${InfoRequerida.rol}<br>
+        Fecha de registro: ${InfoRequerida.fecha_reg}
         `
     }else{
         alert('Operación cancelada')
@@ -83,14 +102,16 @@ btnCategoria.addEventListener('click', (e)=>{
 
     if(EntradaRol != null || EntradaRol != undefined){
 
-        let InfoRequerida = personas.filter((e)=> e.rol.toLowerCase() === EntradaRol.toLocaleLowerCase());
+        let InfoRequerida = JSON.parse(localStorage.getItem('lista')).filter((e)=> e.rol.toLowerCase() === EntradaRol.toLocaleLowerCase());
+        resultado.innerHTML = "";
         InfoRequerida.map(persona =>{
             let InfoPersona = document.createElement('div');
             InfoPersona.classList.add('item-respuesta');
             InfoPersona.innerHTML = `
             Nombre: ${persona.nombre}<br>
             ID: ${persona.id}<br>
-            Rol: <b>${persona.rol}</b>
+            Rol: <b>${persona.rol}</b><br>
+            Fecha de registro: ${persona.fecha_reg}
             `;
             resultado.appendChild(InfoPersona);
         });
